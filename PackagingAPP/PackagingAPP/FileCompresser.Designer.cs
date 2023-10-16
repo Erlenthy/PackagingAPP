0bc81458bd2a2958a522613953e3cd58a9730bee
Dominika Jarocka <140502658+Erlenthy@users.noreply.github.com>
2023-10-16 19:08:13 +0200

    Adding my new project

---
+++ PackagingAPP/PackagingAPP/FileCompresser.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace PackagingAPP
+{
+    partial class FileCompresser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileCompresser));
+            this.przegladajButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.factLabel = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.generateFactButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // przegladajButton
+            // 
+            this.przegladajButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.przegladajButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(106)))), ((int)(((byte)(103)))));
+            this.przegladajButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(200)))), ((int)(((byte)(196)))));
+            this.przegladajButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.przegladajButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.przegladajButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(7)))), ((int)(((byte)(12)))));
+            this.przegladajButton.Location = new System.Drawing.Point(12, 186);
+            this.przegladajButton.Name = "przegladajButton";
+            this.przegladajButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.przegladajButton.Size = new System.Drawing.Size(205, 72);
+            this.przegladajButton.TabIndex = 0;
+            this.przegladajButton.Text = "Compress to one archive";
+            this.przegladajButton.UseVisualStyleBackColor = false;
+            this.przegladajButton.Click += new System.EventHandler(this.przegladajButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // factLabel
+            // 
+            this.factLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.factLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.factLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(7)))), ((int)(((byte)(12)))));
+            this.factLabel.Location = new System.Drawing.Point(92, 50);
+            this.factLabel.Name = "factLabel";
+            this.factLabel.Size = new System.Drawing.Size(290, 100);
+            this.factLabel.TabIndex = 1;
+            this.factLabel.Text = "Miło Cię widzieć :)";
+            this.factLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.factLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(53, 325);
+            this.progressBar1.Maximum = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(372, 25);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // generateFactButton
+            // 
+            this.generateFactButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.generateFactButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(106)))), ((int)(((byte)(103)))));
+            this.generateFactButton.FlatAppearance.BorderSize = 0;
+            this.generateFactButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.generateFactButton.Location = new System.Drawing.Point(181, 296);
+            this.generateFactButton.Name = "generateFactButton";
+            this.generateFactButton.Size = new System.Drawing.Size(116, 23);
+            this.generateFactButton.TabIndex = 3;
+            this.generateFactButton.Text = "New Random Fact";
+            this.generateFactButton.UseVisualStyleBackColor = false;
+            this.generateFactButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(106)))), ((int)(((byte)(103)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(200)))), ((int)(((byte)(196)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(263, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 72);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Compress to separate archives";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // FileCompresser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(77)))), ((int)(((byte)(87)))));
+            this.ClientSize = new System.Drawing.Size(480, 362);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.generateFactButton);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.factLabel);
+            this.Controls.Add(this.przegladajButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FileCompresser";
+            this.Text = "Ziplock";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button przegladajButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label factLabel;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button generateFactButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PackagingAPP/PackagingAPP/FileCompresser.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace PackagingAPP
+{
+    public partial class FileCompresser : Form
+    {
+        private string[] facts =
+        {
+            "Did you know 11% of people are left handed",
+            "Did you know August has the highest percentage of births",
+            "Did you know unless food is mixed with saliva you can't taste it",
+            "Did you know the average person falls asleep in 7 minutes",
+            "Did you know a bear has 42 teeth",
+            "Did you know an ostrich's eye is bigger than its brain",
+            "Did you know lemons contain more sugar than strawberries",
+            "Did you know 8% of people have an extra rib",
+            "Did you know 85% of plant life is found in the ocean",
+            "Did you know Ralph Lauren's original name was Ralph Lifshitz",
+            "Did you know rabbits like licorice",
+            "Did you know the Hawaiian alphabet has 13 letters",
+            "Did you know 'Topolino' is the name for Mickey Mouse Italy",
+            "Did you know a lobsters blood is colorless but when exposed to oxygen it turns blue",
+            "Did you know armadillos have 4 babies at a time and are all the same sex",
+            "Did you know reindeer like bananas",
+            "Did you know the longest recorded flight of a chicken was 13 seconds",
+            "Did you know birds need gravity to swallow",
+            "Did you know the most commonly used letter in the alphabet is E",
+            "Did you know the 3 most common languages in the world are Mandarin Chinese, Spanish and English",
+            "Did you know dreamt is the only word that ends in mt",
+            "Did you know the first letters of the months July through to November spell JASON",
+            "Did you know a cat has 32 muscles in each ear",
+            "Did you know Perth is Australia's windiest city",
+            "Did you know Elvis's middle name was Aron",
+            "Did you know goldfish can see both infrared and ultraviolet light",
+            "Did you know the smallest bones in the human body are found in your ear",
+            "Did you know cats spend 66% of their life asleep",
+            "Did you know Switzerland eats the most chocolate equating to 10 kilos per person per year",
+            "Did you know money is the number one thing that couples argue about",
+            "Did you know macadamia nuts are toxic to dogs",
+            "Did you know when lightning strikes it can reach up to 30,000 degrees celsius (54,000 degrees fahrenheit)",
+            "Did you know spiders are arachnids and not insects",
+            "Did you know each time you see a full moon you always see the same side",
+            "Did you know stewardesses is the longest word that is typed with only the left hand",
+            "Did you know honey is the only natural food which never spoils",
+            "Did you know M&M's chocolate stands for the initials for its inventors Mars and Murrie",
+            "Did you know that you burn more calories eating celery than it contains (the more you eat the thinner you become)",
+            "Did you know the only continent with no active volcanoes is Australia",
+            "Did you know the longest street in the world is Yonge street in Toronto Canada measuring 1,896 km (1,178 miles)",
+
+
+
+
+
+
+
+                
+            // Dodaj więcej faktów do wyświetlenia
+
+        };
+
+        private int maxClicks = 10; // Maksymalna liczba kliknięć
+        private int currentClicks = 0; // Aktualna liczba kliknięć
+
+        private Random random = new Random();
+
+        private Stopwatch stopwatch = new Stopwatch(); // Stopwatch do śledzenia czasu
+
+        public FileCompresser()
+        {
+
+            InitializeComponent();
+            DisplayRandomFact();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = maxClicks;
+            stopwatch.Start();
+        }
+
+        private void DisplayRandomFact()
+        {
+            // Wylosuj indeks losowego faktu
+            int randomIndex = random.Next(facts.Length);
+
+            // Wyświetl wybrany fakt w Label
+            factLabel.Text = facts[randomIndex];
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Rozpocznij odliczanie czasu po załadowaniu formularza
+            stopwatch.Start();
+        }
+
+        private void przegladajButton_Click(object sender, EventArgs e)
+        {
+
+
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "C:\\"; // Możesz ustawić początkowy katalog
+                openFileDialog.Filter = "Wszystkie pliki (*.*)|*.*"; // Filtr na wszystkie pliki
+                openFileDialog.FilterIndex = 1; // Indeks domyślnego filtru
+                openFileDialog.Multiselect = true;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string[] selectedFilePaths = openFileDialog.FileNames; // Pobierz tablicę wybranych ścieżek plików
+
+
+                    string sevenZipPath = Find7ZipPath();
+                    if (sevenZipPath != null)
+                    {
+                        // Pobierz ścieżkę do folderu "Dokumenty" użytkownika
+                        string documentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                        string timestamp = DateTime.Now.ToString("dd_MM_yyyy_HH_mm_ss");
+
+
+
+                        // Utwórz ścieżkę docelową (folder "SkompresowaneAPP" w folderze "Dokumenty")
+                        string targetAppFolder = Path.Combine(documentsFolder, "SkompresowaneAPP");
+                        string targetDataFolder = Path.Combine(targetAppFolder, $"Compressed_{timestamp}");
+
+                        // Upewnij się, że folder docelowy istnieje lub go utwórz
+                        Directory.CreateDirectory(targetAppFolder);
+                        Directory.CreateDirectory(targetDataFolder);
+
+
+
+                        string randomPassword = GenerateRandomPassword();
+
+
+                        // Utwórz nazwę pliku na podstawie daty i godziny
+                        string targetFileName = $"{timestamp}.zip";
+
+                        // Pełna ścieżka do pliku docelowego
+                        string targetFilePath = Path.Combine(targetAppFolder, targetDataFolder, targetFileName);
+
+                        string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                        int length = 12;
+                        Random random = new Random();
+                        string passwordFilePath = Path.Combine(targetDataFolder, "IAmNotAPassword.txt");
+                        File.WriteAllText(passwordFilePath, randomPassword);
+
+                        // Wywołaj 7-Zip, korzystając ze znalezionej ścieżki
+                        ProcessStartInfo psi = new ProcessStartInfo();
+                        psi.FileName = sevenZipPath;
+                        string filesToCompress = string.Join(" ", selectedFilePaths.Select(path => $"\"{path}\""));
+                        psi.Arguments = $"a -p{randomPassword} -mx5 -tzip \"{targetFilePath}\" {filesToCompress}"; // -mx9 oznacza najwyższy poziom kompresji
+                        psi.UseShellExecute = false;
+                        psi.CreateNoWindow = true;
+                        psi.RedirectStandardOutput = true;
+                        psi.RedirectStandardError = true;
+
+                        using (Process process = new Process())
+                        {
+                            process.StartInfo = psi;
+                            process.Start();
+                            process.WaitForExit();
+
+                            if (process.ExitCode == 0)
+                            {
+                                MessageBox.Show($"Plik został skompresowany i zapisany jako {targetFileName} w folderze Dokumenty\\SkompresowaneAPP.");
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Wystąpił błąd podczas kompresji pliku.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nie znaleziono 7-Zip na tym komputerze. Zainstaluj 7-Zip lub podaj własną ścieżkę do programu.");
+                    }
+                }
+                string GenerateRandomPassword()
+                {
+                    string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                    int length = 12;
+                    Random random = new Random();
+                    string password = new string(Enumerable.Repeat(characters, length)
+                        .Select(s => s[random.Next(s.Length)]).ToArray());
+                    return password;
+                }
+            }
+        }
+
+
+
+        private string Find7ZipPath()
+        {
+            // Standardowe lokalizacje, gdzie może znajdować się 7-Zip
+            string[] possiblePaths = {
+        @"C:\Program Files\7-Zip\7z.exe",
+        @"C:\Program Files (x86)\7-Zip\7z.exe",
+        @"C:\Program Files\7-Zip\7zFM.exe",
+        @"C:\Program Files (x86)\7-Zip\7zFM.exe"
+    };
+
+            foreach (string path in possiblePaths)
+            {
+                if (File.Exists(path))
+                {
+                    return path;
+                }
+            }
+
+            return null; // Jeśli nie znaleziono 7-Zip
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            // Inkrementuj liczbę kliknięć
+            currentClicks++;
+
+            // Zaktualizuj wartość ProgressBar
+            progressBar1.Value = currentClicks;
+
+            // Sprawdź, czy osiągnięto maksymalną wartość
+            if (currentClicks >= maxClicks)
+            {
+                // Zatrzymaj odliczanie czasu i oblicz czas spędzony w aplikacji
+                stopwatch.Stop();
+
+
+                // Tutaj możesz wykonać odpowiednie działania po osiągnięciu maksimum
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Wylosuj indeks losowego faktu
+            int randomIndex = random.Next(facts.Length);
+
+            // Wyświetl wybrany fakt w kontrolce Label
+            factLabel.Text = facts[randomIndex];
+
+            // Inkrementuj liczbę kliknięć
+            currentClicks++;
+
+            // Zaktualizuj wartość ProgressBar
+            progressBar1.Value = currentClicks;
+
+
+
+
+            // Sprawdź, czy osiągnięto maksymalną wartość
+            if (currentClicks >= maxClicks)
+            {
+                TimeSpan elapsedTime = stopwatch.Elapsed;
+                MessageBox.Show($"Did your supervisior know u spend {elapsedTime} on random facts instead of a work?");
+                // Tutaj możesz wykonać odpowiednie działania po osiągnięciu maksimum
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "C:\\"; // Możesz ustawić początkowy katalog
+                openFileDialog.Filter = "Wszystkie pliki (*.*)|*.*"; // Filtr na wszystkie pliki
+                openFileDialog.FilterIndex = 1; // Indeks domyślnego filtru
+                openFileDialog.Multiselect = true;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string[] selectedFilePaths = openFileDialog.FileNames; // Pobierz tablicę wybranych ścieżek plików
+
+
+                    string sevenZipPath = Find7ZipPath();
+                    if (sevenZipPath != null)
+                    {
+                        // Pobierz ścieżkę do folderu "Dokumenty" użytkownika
+                        string documentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                        string timestamp = DateTime.Now.ToString("dd_MM_yyyy");
+
+
+                
+                        // Utwórz ścieżkę docelową (folder "SkompresowaneAPP" w folderze "Dokumenty")
+                        string targetAppFolder = Path.Combine(documentsFolder, "SkompresowaneAPP");
+                        string targetDataFolder = Path.Combine(targetAppFolder, $"Compressed_{timestamp}");
+
+                        // Upewnij się, że folder docelowy istnieje lub go utwórz
+                        Directory.CreateDirectory(targetAppFolder);
+                        Directory.CreateDirectory(targetDataFolder);
+
+
+                        foreach (string selectedFilePath in selectedFilePaths)
+                        {
+
+                        
+                                string randomPassword = GenerateRandomPassword();
+                            string originalFileNameWithoutExtension = Path.GetFileNameWithoutExtension(selectedFilePath);
+                            string folderName = $"{timestamp}_{originalFileNameWithoutExtension}";
+
+                            string compressedAppFolder = Path.Combine(documentsFolder, "SkompresowaneAPP", targetDataFolder );
+                            string compressedDataFolder = Path.Combine(compressedAppFolder, folderName);
+
+
+                            // Utwórz nazwę pliku na podstawie daty i godziny
+                            string targetFileName = $"{timestamp}_{Path.GetFileNameWithoutExtension(selectedFilePath)}.zip";
+                            string passwordFilePath = Path.Combine(targetDataFolder, compressedDataFolder, $"{Path.GetFileNameWithoutExtension(selectedFilePath)}.txt");
+
+                            Directory.CreateDirectory(compressedAppFolder);
+                            Directory.CreateDirectory(compressedDataFolder);
+
+                            // Pełna ścieżka do pliku docelowego
+                            string targetFilePath = Path.Combine(targetDataFolder,compressedDataFolder, targetFileName);
+                            File.WriteAllText(passwordFilePath, randomPassword);
+
+                            string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                            int length = 12;
+                            Random random = new Random();
+                            
+                            File.WriteAllText(passwordFilePath, randomPassword);
+
+
+
+                            // Wywołaj 7-Zip, korzystając ze znalezionej ścieżki
+                            ProcessStartInfo psi = new ProcessStartInfo();
+                                psi.FileName = sevenZipPath;
+
+                                psi.Arguments = $"a -p{randomPassword} -mx5 -tzip \"{targetFilePath}\" \"{selectedFilePath}\""; // -mx9 oznacza najwyższy poziom kompresji
+                                psi.UseShellExecute = false;
+                                psi.CreateNoWindow = true;
+                                psi.RedirectStandardOutput = true;
+                                psi.RedirectStandardError = true;
+
+                                using (Process process = new Process())
+                                {
+                                    process.StartInfo = psi;
+                                    process.Start();
+                                    process.WaitForExit();
+
+                                    if (process.ExitCode == 0)
+                                    {
+                                        MessageBox.Show($"Plik został skompresowany i zapisany jako {targetFileName} w folderze Dokumenty\\SkompresowaneAPP\\{folderName}.");
+
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Wystąpił błąd podczas kompresji pliku.");
+                                    }
+                                }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nie znaleziono 7-Zip na tym komputerze. Zainstaluj 7-Zip lub podaj własną ścieżkę do programu.");
+                    }
+                }
+                string GenerateRandomPassword()
+                {
+                    string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                    int length = 12;
+                    Random random = new Random();
+                    string password = new string(Enumerable.Repeat(characters, length)
+                        .Select(s => s[random.Next(s.Length)]).ToArray());
+                    return password;
+                }
+            }
+        }
+        string GenerateRandomPassword()
+        {
+            string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            int length = 12;
+            Random random = new Random();
+            string password = new string(Enumerable.Repeat(characters, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+            return password;
+        }
+        
+    }
+
+}
+
+
+        
+        
+    
+
